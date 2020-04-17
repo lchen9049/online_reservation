68d6a160726e8d6c239f8a269a52dae9b2126f06
Linda Chen <lchen9049@gmail.com>
2020-04-17 15:21:20 -0500

    DDL Format changes

--- DDL.sql
+++ DDL.sql
@@ -18,30 +18,30 @@ create table staff
 
 create table customers
 	(
-		username			varchar(50), 
-		first_name		    varchar(50),
-        last_name		    varchar(50), 
+        username            varchar(50), 
+        first_name          varchar(50),
+        last_name           varchar(50), 
         member              boolean, 
         car_plate           varchar(20),
-		phone_number		varchar(20),
+        phone_number        varchar(20),
         start_time          numeric(2),
         end_time            numeric(2),
         spot_id             varchar(3),
         monthly_fee         numeric(5),
-		primary key (username)
+        primary key (username)
 	);
 
 create table credit_cards
     (
-        username			varchar(50),
-        billing_address		varchar(100),
- 		card_number			varchar(20),
- 		card_pin			varchar(3),
+        username            varchar(50),
+        billing_address	    varchar(100),
+        card_number         varchar(20),
+        card_pin            varchar(3),
         city                varchar(10),
         state               varchar(10),
         zip_code            varchar(10),
         primary key (username, card_number),
-		foreign key (username) references users (username)
+        foreign key (username) references users (username)
     );
 
 create table spots
